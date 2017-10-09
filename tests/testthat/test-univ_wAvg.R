87cf06c363ed7a65b81be474b270a309f7b1d2d1
HBossier <bossier.han@gmail.com>
2017-10-09 17:29:53 +0100

    Adding test folder and description for it.
    Adding test folder that will contain a test for the calculation
    of the weighted average.

---
+++ tests/testthat/test-univ_wAvg.R
@@ -0,0 +1,13 @@
+context("Weighted Average - Univariate MA")
+
+test_that("Calculating a weighted average", {
+  # Random t-values of 5 studies
+  tVal <- c(1,3,3,5,7)
+  # Sample sizes
+  nsub <- c(10,12,14,16,18)
+
+  # Calculate g
+  heG <- hedgeG()
+
+}
+
