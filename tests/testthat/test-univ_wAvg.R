f0541416887e52747b9c10945927fcea60582b25
HBossier <bossier.han@gmail.com>
2017-10-09 18:19:48 +0100

    Adding tests for Hedges g, variance, tau and weighted average.
    These tests take a known data frame, calculate g, var g, tau,
    the weighted average using the functions from the package and then
    compare with the expected value.

--- tests/testthat/test-univ_wAvg.R
+++ tests/testthat/test-univ_wAvg.R
@@ -1,13 +1,30 @@
+library(dplyr)
 context("Weighted Average - Univariate MA")
 
-test_that("Calculating a weighted average", {
-  # Random t-values of 5 studies
-  tVal <- c(1,3,3,5,7)
-  # Sample sizes
-  nsub <- c(10,12,14,16,18)
+# Start Data: random t-values from 5 studies
+test_data <- data.frame(tVal = c(1,3,3,5,7),
+                        nsub = c(10,12,14,16,18))
 
-  # Calculate g
-  heG <- hedgeG()
+test_that("g, var(g), tau and weighted average are correct", {
+  # Calculate g and variance using functions hedgeG and varHedge
+  heG <- mutate(test_data,
+                g = hedgeG(t = tVal, N = nsub),
+                varG = varHedge(g = g, N = nsub))
 
-}
+  # Between study
+  tauEs <- summarise(heG, tau = tau(Y = g, W = (1/varG), k = 5))
+
+  # Weighted average
+  wAvg <- cbind(heG, tauEs) %>% mutate(weights = 1/(varG + tau)) %>%
+    summarise(wAvg = wMean(Y = g, W = weights))
+
+  # Now test the mean of g, var(g), tau and weighted average
+  expect_equal(round(mean(heG$g),3), 0.922)
+  expect_equal(round(mean(heG$varG),3), 0.115)
+  expect_equal(unlist(round(tauEs,3)), c(tau = 0.110))
+  expect_equal(unlist(round(wAvg,3)), c(wAvg = 0.903))
+})
+
+
+
 
